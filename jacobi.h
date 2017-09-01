47c1e9649080d7800d3e6a9069ddd9a6ff925f1e
Marco <m.barbone@outlook.com>
2017-09-01 19:06:43 +0200

    implemented serial jacobi

--- jacobi.h
+++ jacobi.h
@@ -5,6 +5,9 @@
 #ifndef JACOBI_JACOBI_H
 #define JACOBI_JACOBI_H
 
+#include <vector>
+#include <cmath>
+
 /*
 
 n = no of equations
@@ -16,9 +19,45 @@ x[n] - solution vector
 */
 
 template<typename T>
-std::vector <T> jacobi(std::vector <std::vector<T>> coefficients, std::vector <T> right_terms, ulong iterations,
-                       T tolerance) {
+inline T max(T a, T b) { return (a > b) ? a : b; }
 
+template<typename T>
+inline T solution_find(const std::vector<T> row, const std::vector<T> solutions, T term, const ulong index) {
+    for (int j = 0; j < row.size(); ++j) {
+        if (j == index) continue;
+        term -= solutions[j] * row[j];
+    }
+    return term / row[index];
+}
+
+template<typename T>
+std::vector<T> serial_jacobi(const std::vector<std::vector<T>> coefficients, const std::vector<T> terms,
+                             const ulong iterations, const T tolerance) {
+
+    std::vector<T> old_solutions(coefficients.size(), (tolerance - tolerance));
+    std::vector<T> solutions(coefficients.size(), (tolerance - tolerance));
+    T error;
+    for (ulong i = 0; i < solutions.size(); ++i)
+        solutions[i] = solution_find(coefficients[i], old_solutions, terms[i], i);
+
+    for (int iteration = 1; iteration < iterations; ++iteration) {
+        for (ulong i = 0; i < solutions.size(); ++i)
+            solutions[i] = solution_find(coefficients[i], old_solutions, terms[i], i);
+
+        for (auto &sol: solutions) {
+            std::cout << sol << " ";
+        }
+        std::cout << std::endl;
+
+        error = abs(solutions[0] - old_solutions[0]);
+        for (int i = 1; i < solutions.size(); ++i) {
+            error = max<T>(abs(solutions[i] - old_solutions[i]), error);
+            old_solutions[i] = solutions[i];
+        }
+        if (error <= tolerance) return solutions;
+    }
+
+    return solutions;
 }
 
 #endif //JACOBI_JACOBI_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 #include "jacobi.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    double matrix[4][4] = {
+            {10., -1., 2.,  0.},
+            {-1., 11., -1., 3.},
+            {2.,  -1., 10., -1.},
+            {0.0, 3.,  -1., 8.}
+    };
+    double term[] = {6., 25., -11., 15.};
+
+    vector<vector<double>> coefficients(4, vector<double>(4, 0.));
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            coefficients[i][j] = matrix[i][j];
+        }
+    }
+    vector<double> terms;
+    for (int i = 0; i < 4; ++i) {
+        terms.emplace_back(term[i]);
+    }
+    vector<double> solutions = serial_jacobi(coefficients, terms, 1000, 0.0);
+
+    cout << "solution: ";
+    for (auto &sol: solutions) {
+        cout << sol << " ";
+    }
+    cout << endl;
+
     return 0;
 }
