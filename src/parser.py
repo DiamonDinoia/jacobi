565777e85ce5269d33c7f5113de5539666ba0034
Marco <m.barbone@outlook.com>
2017-11-10 15:33:19 +0100

    Refactoring

--- src/parser.py
+++ src/parser.py
@@ -15,6 +15,7 @@ algorithm = "algorithm"
 workers = "workers"
 init_time = "initialization time"
 comp_time = "computation time"
+total_time = "total time"
 iterations = "iterations computed"
 error = "error"
 
@@ -27,8 +28,8 @@ labels = [algorithm, matrix_size, workers, init_time, comp_time, iterations, err
 def line_graph(frame, label, xticklabel, ylabel, xlabel, figname):
     col = [x for x in labels if label in x]
     if init_time in col:
-        frame['total time'] = frame[col[0]] + frame[col[1]]
-        col.append('total time')
+        frame[total_time] = frame[col[0]] + frame[col[1]]
+        col.append(total_time)
     a = frame[col].plot(kind='line', linewidth=2.0, rot=0, grid=True)
     if workers == xticklabel:
         a.set_xticks(frame[xticklabel])
@@ -41,7 +42,7 @@ def line_graph(frame, label, xticklabel, ylabel, xlabel, figname):
         plt.xlim(1, len(frame[xticklabel]))
     else:
         a.set_xticklabels(frame[xticklabel])
-    a.set_yticks(frame['total time'])
+    a.set_yticks(frame[total_time])
     locator = ticker.AutoLocator()
     a.yaxis.set_major_locator(locator)
     plt.legend(col, loc='best')
@@ -49,6 +50,7 @@ def line_graph(frame, label, xticklabel, ylabel, xlabel, figname):
     plt.xlabel(ylabel)
     plt.title(figname)
     plt.savefig(figname)
+    plt.clf()
     # plt.show()
 
 
@@ -76,6 +78,18 @@ def get_data(metric, filename, label, xticklabel, ylabel, xlabel, figname):
         sequential_data[metric] = frame
 
 
+def line_metric_graph(frame, label, xticklabel, ylabel, xlabel, figname):
+    frame[label].plot(kind='line', linewidth=2.0, rot=0, grid=True)
+    if label == 'efficiency':
+        plt.ylim([0, 1])
+    plt.ylabel(xlabel)
+    plt.xlabel(ylabel)
+    plt.title(figname if 'xxl' not in figname else 'efficiency')
+    plt.savefig(figname)
+    plt.clf()
+    # plt.show()
+
+
 def main():
     os.chdir(directory)
     for filename in glob(filetype):
@@ -91,6 +105,26 @@ def main():
         else:
             metric = 'workers'
             get_data('xxl', filename, 'time', metric, metric, 'time', 'workers with large matrix')
+
+    time = sequential_data['size'][sequential_data['size']['matrix_size'] == 1024][total_time].iloc[0]
+
+    frames = [omp_data, thread_data, ff_data]
+    for frame in frames:
+        frame[workers]['ideal time'] = [time / x for x in frame[workers][workers]]
+        frame[workers]['efficiency'] = frame[workers]['ideal time'] / frame[workers][total_time]
+    line_metric_graph(omp_data[workers], 'efficiency', workers, 'efficiency', workers, 'omp efficiency')
+    line_metric_graph(thread_data[workers], 'efficiency', workers, 'efficiency', workers, 'thread efficiency')
+    line_metric_graph(ff_data[workers], 'efficiency', workers, 'efficiency', workers, 'fastflow efficiency')
+
+    time = sequential_data['size'][sequential_data['size']['matrix_size'] == 16384][total_time].iloc[0]
+
+    for frame in frames:
+        frame['xxl']['ideal time'] = [time / x for x in frame['xxl'][workers]]
+        frame['xxl']['efficiency'] = frame['xxl']['ideal time'] / frame['xxl'][total_time]
+        # print frame
+    line_metric_graph(omp_data['xxl'], 'efficiency', workers, 'efficiency', workers, 'omp xxl efficiency')
+    line_metric_graph(thread_data['xxl'], 'efficiency', workers, 'efficiency', workers, 'thread xxl efficiency')
+    line_metric_graph(ff_data['xxl'], 'efficiency', workers, 'efficiency', workers, 'fastflow xxl efficiency')
 
 
 if __name__ == "__main__":
