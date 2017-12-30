885adcb21d835ceceba2c5d40f619710221a6c29
Marco <m.barbone@outlook.com>
2017-12-30 16:02:02 +0100

    Modified parser and graphs

--- src/parser.py
+++ src/parser.py
@@ -19,18 +19,25 @@ total_time = "total time"
 iterations = "iterations computed"
 error = "error"
 
-directory = "../data"
+directory = "./data"
 filetype = "*.csv"
 
 labels = [algorithm, matrix_size, workers, init_time, comp_time, iterations, error]
 
 
-def line_graph(frame, label, xticklabel, ylabel, xlabel, figname):
+def line_graph(frame, label, xticklabel, ylabel, xlabel, figname, ideal=None):
     col = [x for x in labels if label in x]
+    if label == total_time:
+        col = [total_time]
     if init_time in col:
         frame[total_time] = frame[col[0]] + frame[col[1]]
         col.append(total_time)
-    a = frame[col].plot(kind='line', linewidth=2.0, rot=0, grid=True)
+    if ideal is None:
+        a = frame[col].plot(kind='line', linewidth=2.0, rot=0, grid=True)
+    else:
+        b = ideal[col].plot(kind='line', linewidth=2.0, rot=0, grid=True)
+        a = frame[col].plot(kind='line', linewidth=2.0, rot=0, grid=True, ax=b)
+        # print(ideal[col],frame[col])
     if workers == xticklabel:
         a.set_xticks(frame[xticklabel])
         locator = ticker.AutoLocator()
@@ -45,13 +52,18 @@ def line_graph(frame, label, xticklabel, ylabel, xlabel, figname):
     a.set_yticks(frame[total_time])
     locator = ticker.AutoLocator()
     a.yaxis.set_major_locator(locator)
-    plt.legend(col, loc='best')
+    tmp = figname.split(' ')
+    if ideal is None:
+        plt.legend(col, loc='best')
+    else:
+        plt.legend(['Model', tmp[0]], loc='best')
     plt.ylabel(xlabel)
     plt.xlabel(ylabel)
-    tmp = figname.split(' ')
     plt.title(tmp[0])
     plt.savefig(figname)
+    print(figname)
     plt.clf()
+    plt.close()
     # plt.show()
 
 
@@ -95,8 +107,21 @@ def line_metric_graph(frame, label, xticklabel, ylabel, xlabel, figname):
     tmp = figname.split(' ')
     plt.title(tmp[0])
     plt.savefig(' '.join(tmp))
+    print(figname)
     plt.clf()
+    plt.close()
     # plt.show()
+
+
+def evaluate_model(k, m, n, mult, thread_cost=0., alloc_cost=0.):
+    frame = pd.DataFrame(columns=[matrix_size, workers, init_time, comp_time, total_time, iterations])
+
+    for i in range(1, n + 1):
+        init_cost = thread_cost * i + alloc_cost * m
+        calc = k * mult * m * m / i
+        values = [m, i, init_cost, calc, init_cost + calc, k]
+        frame.loc[i] = values
+    return frame
 
 
 def main():
@@ -115,22 +140,44 @@ def main():
             metric = 'workers'
             get_data('xxl', filename, 'time', metric, metric, 'time', 'workers with large matrix')
 
-    time = sequential_data['size'][sequential_data['size']['matrix_size'] == 1024][total_time].iloc[0]
+    multiplication_cost = sequential_data['size'][sequential_data['size'][matrix_size] == 16384][comp_time].iloc[0] / (
+            50 * 16384 * 16384)
+    thread_cost = thread_data['xxl'][thread_data['xxl']['workers'] == 128][init_time].iloc[0]
+    thread_cost -= thread_data['xxl'][thread_data['xxl']['workers'] == 1][init_time].iloc[0]
+    thread_cost /= 128
+    alloc_cost = (thread_data['workers'][thread_data['workers']['workers'] == 1][init_time].iloc[
+                      0] - thread_cost) / 1024
+
+    time = sequential_data['size'][sequential_data['size']['matrix_size'] == 1024][comp_time].iloc[0]
+    init = sequential_data['size'][sequential_data['size']['matrix_size'] == 1024][init_time].iloc[0]
+
+    idealframe = evaluate_model(50, 1024, 128, multiplication_cost, 0.382702 / 1024, 0.050056 / 1638400)
+    line_graph(idealframe, 'time', 'workers', 'workers', 'time', 'Model time 1024')
 
     frames = [omp_data, thread_data, ff_data]
     for frame in frames:
-        frame[workers]['ideal time'] = [time / x for x in frame[workers][workers]]
+        frame[workers]['ideal time'] = [(time / x) + init for x in frame[workers][workers]]
         frame[workers]['efficiency'] = frame[workers]['ideal time'] / frame[workers][total_time]
+
+    line_graph(omp_data[workers], total_time, 'workers', 'workers', 'time', 'OpenMP comparision', idealframe)
+    line_graph(thread_data[workers], total_time, 'workers', 'workers', 'time', 'Threads comparision', idealframe)
+    line_graph(ff_data[workers], total_time, 'workers', 'workers', 'time', 'FastFlow comparision', idealframe)
     line_metric_graph(omp_data[workers], 'efficiency', workers, 'efficiency', workers, 'OpenMP efficiency')
     line_metric_graph(thread_data[workers], 'efficiency', workers, 'efficiency', workers, 'Threads efficiency')
     line_metric_graph(ff_data[workers], 'efficiency', workers, 'efficiency', workers, 'FastFlow efficiency')
 
-    time = sequential_data['size'][sequential_data['size']['matrix_size'] == 16384][total_time].iloc[0]
+    time = sequential_data['size'][sequential_data['size']['matrix_size'] == 16384][comp_time].iloc[0]
+    init = sequential_data['size'][sequential_data['size']['matrix_size'] == 16384][init_time].iloc[0]
+    idealframe = evaluate_model(50, 16384, 128, multiplication_cost, 0.382702 / 1024, 0.050056 / 1638400)
+    line_graph(idealframe, 'time', 'workers', 'workers', 'time', 'Model time 16384')
 
     for frame in frames:
-        frame['xxl']['ideal time'] = [time / x for x in frame['xxl'][workers]]
+        frame['xxl']['ideal time'] = [(time / x) + init for x in frame['xxl'][workers]]
         frame['xxl']['efficiency'] = frame['xxl']['ideal time'] / frame['xxl'][total_time]
         # print frame
+    line_graph(omp_data['xxl'], total_time, 'workers', 'workers', 'time', 'OpenMP xxl comparision', idealframe)
+    line_graph(thread_data['xxl'], total_time, 'workers', 'workers', 'time', 'Threads xxl comparision', idealframe)
+    line_graph(ff_data['xxl'], total_time, 'workers', 'workers', 'time', 'FastFlow xxl comparision', idealframe)
     line_metric_graph(omp_data['xxl'], 'efficiency', workers, 'efficiency', workers, 'OpenMP xxl efficiency')
     line_metric_graph(thread_data['xxl'], 'efficiency', workers, 'efficiency', workers, 'Threads xxl efficiency')
     line_metric_graph(ff_data['xxl'], 'efficiency', workers, 'efficiency', workers, 'FastFlow xxl efficiency')
